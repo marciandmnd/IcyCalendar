2ac975ee421a8cb4da30a725542ad83f16dff811
Marcian Diamond <marciandmnd@yahoo.com>
2017-02-07 13:23:36 -0800

    Removed index action from calendars controller

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -1,28 +1,5 @@
 class CalendarsController < ApplicationController
 	before_action :set_year_and_month, only: [:index, :show, :show_day]
-	def index
-		@next_month = @month == 12 ? 1 : @month + 1
-		@prev_month =  @month == 1 ? 12 : @month - 1
-
-		@current_month = Time.new(@year, @month,1).to_date
-		@current_date = Time.new
-
-		@current_date.month == @month && @current_date.year == @year ? @today = @current_date.day : nil
-
-		@calendar_cell_index = 0;
-		@day_index = 1;
-		@first_weekday_of_month = @current_month.wday 
-		@num_weeks_in_month = (@num_days_in_month.to_f / 7).ceil
-		@num_days_in_month = Time::days_in_month(@current_month.month, @current_month.year)
-
-		@appointments = Appointment.where('extract(year from date) = ? AND extract(month from date) = ? AND user_id = ?', @year, @month, current_or_guest_user.id)
-		@appointments_indices = Hash[(0...@num_days_in_month).map { |num| [num,  0] }]
-
-		@appointments.each do |appointment|
-			@appointments_indices[appointment.date.day-1] += 1
-		end
-
-	end
 
 	def show
 		@next_month = @month == 12 ? 1 : @month + 1
@@ -37,6 +14,9 @@ class CalendarsController < ApplicationController
 		@first_weekday_of_month = @current_month.wday 
 		@num_weeks_in_month = (@num_days_in_month.to_f / 7).ceil
 		@num_days_in_month = Time::days_in_month(@current_month.month, @current_month.year)
+
+		@appointments = Appointment.where('extract(year from date) = ? AND extract(month from date) = ? AND user_id = ?', @year, @month, current_user.id)
+		byebug
 	end
 
 	def show_day
