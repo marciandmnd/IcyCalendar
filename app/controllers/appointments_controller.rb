4ebc5135f0102084d2cd000a5336d6cb9aca9db2
Marcian Diamond <marciandmnd@yahoo.com>
2017-11-05 17:31:12 -0800

    Update appointment creation/editing logic

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -41,11 +41,46 @@ class AppointmentsController < ApplicationController
     @year = params[:year].to_i
     @month = params[:month].to_i
     @date = Time.new(@year,@month,@day)
+    # byebug
   end
 
   def update
     @appointment = Appointment.find(params[:id])
+
+    appt_date = DateTime.strptime(params[:appointment][:date], "%Y-%m-%d")
+
+    appt_year = appt_date.year
+    appt_month = appt_date.month
+    appt_day = appt_date.day
+
+    date_from_hour = params[:appointment]["date_from(4i)"].to_i
+    date_from_minute = params[:appointment]["date_from(5i)"].to_i
+    date_from = DateTime.new(appt_year, appt_month, appt_day, date_from_hour, date_from_minute)
+
+    date_to_hour = params[:appointment]["date_to(4i)"].to_i
+    date_to_minute = params[:appointment]["date_to(5i)"].to_i
+    date_to = DateTime.new(appt_year, appt_month, appt_day, date_from_hour, date_from_minute)
+    
+    @appointment.date_from = date_from
+    @appointment.date_to = date_to
+    # appt_date = DateTime.strptime(params[:appointment][:date], "%Y-%m-%d")
+
+    # appt_year = appt_date.year
+    # appt_month = appt_date.month
+    # appt_day = appt_date.day
+
+    # date_from_hour = params[:appointment]["date_from(4i)"].to_i
+    # date_from_minute = params[:appointment]["date_from(5i)"].to_i
+    # date_from = DateTime.new(appt_year, appt_month, appt_day, date_from_hour, date_from_minute)
+
+    # date_to_hour = params[:appointment]["date_to(4i)"].to_i
+    # date_to_minute = params[:appointment]["date_to(5i)"].to_i
+    # date_to = DateTime.new(appt_year, appt_month, appt_day, date_from_hour, date_from_minute)
+    
+    # @appointment.date_from = date_from
+    # @appointment.date_to = date_to
     @appointment.update(appointment_params)
+    
     redirect_to show_day_path(@appointment.date_from.year, @appointment.date_from.month, @appointment.date_from.day)
   end
 
